ff6db1a3857d48f9238cfee65f061715eec63621
Luke Camelo <azathothbeats@gmail.com>
2018-11-25 03:23:38 -0500

    Added smoothscroll and slideout CDN

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const path = form.getAttribute('action')
 const responseDiv = document.querySelector('#submit-response')
 const submitButton = document.querySelector('#form-submit')
 
+submitButton.blur()
+form.blur()
+responseDiv.blur()
+
+// const about = document.querySelector('#about-nav')
+// const work = document.querySelector('#work-nav')
+// const contact = document.querySelector('#contact-nav')
+
+// work.addEventListener('click', {
+//   // ...
+// })
+
 form.addEventListener('submit', e => {
   e.preventDefault()
 
@@ -67,3 +79,58 @@ function handleFirstTab(e) {
 }
 
 window.addEventListener('keydown', handleFirstTab)
+
+// SMOOTH SCROLL STUFF
+
+function currentYPosition() {
+  // Firefox, Chrome, Opera, Safari
+  if (self.pageYOffset) return self.pageYOffset
+  // Internet Explorer 6 - standards mode
+  if (document.documentElement && document.documentElement.scrollTop)
+    return document.documentElement.scrollTop
+  // Internet Explorer 6, 7 and 8
+  if (document.body.scrollTop) return document.body.scrollTop
+  return 0
+}
+
+function elmYPosition(eID) {
+  var elm = document.getElementById(eID)
+  var y = elm.offsetTop
+  var node = elm
+  while (node.offsetParent && node.offsetParent != document.body) {
+    node = node.offsetParent
+    y += node.offsetTop
+  }
+  return y
+}
+
+function smoothScroll(eID) {
+  var startY = currentYPosition()
+  var stopY = elmYPosition(eID)
+  var distance = stopY > startY ? stopY - startY : startY - stopY
+  if (distance < 100) {
+    scrollTo(0, stopY)
+    return
+  }
+  var speed = Math.round(distance / 100)
+  if (speed >= 20) speed = 20
+  var step = Math.round(distance / 25)
+  var leapY = stopY > startY ? startY + step : startY - step
+  var timer = 0
+  if (stopY > startY) {
+    for (var i = startY; i < stopY; i += step) {
+      setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed)
+      leapY += step
+      if (leapY > stopY) leapY = stopY
+      timer++
+    }
+    return
+  }
+  for (var i = startY; i > stopY; i -= step) {
+    setTimeout('window.scrollTo(0, ' + leapY + ')', timer * speed)
+    leapY -= step
+    if (leapY < stopY) leapY = stopY
+    timer++
+  }
+  return false
+}
